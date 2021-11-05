17e27e8445567a6444df018b1ce5e8c56369245a
Tien Tran <tientq@outlook.com>
2021-11-05 14:44:38 +0700

    remove etc

--- Formula/confluent-platform.rb
+++ Formula/confluent-platform.rb
@@ -15,10 +15,6 @@ class ConfluentPlatform < Formula
   conflicts_with "kafka", because: "kafka also ships with identically named Kafka related executables"
 
   def install
-    pkgetc.install Dir["etc/**/*", "etc/*"]
-    (buildpath/"etc").rmdir
-    libexec.install_symlink pkgetc => "etc"
-
     libexec.install %w[bin etc libexec share]
     rm_rf libexec/"bin/windows"
 
