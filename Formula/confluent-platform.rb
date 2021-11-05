3a9af9a06546be56e2bb91039bffb860b7bbfdde
Tien Tran <tientq@outlook.com>
2021-11-05 14:35:39 +0700

    etc

--- Formula/confluent-platform.rb
+++ Formula/confluent-platform.rb
@@ -22,7 +22,7 @@ class ConfluentPlatform < Formula
     # "cd: ../Cellar/confluent-platform/5.5.0/bin/../share/java: No such file or directory"
     inreplace libexec/"bin/confluent-hub", "[ -L /usr/local/bin/confluent-hub ]", "false"
 
-    pkgetc.install Dir["etc/*"]
+    pkgetc.install Dir["#{libexec}/etc/*"]
     (buildpath/"etc").rmdir
     libexec.install_symlink pkgetc => "etc"
 
